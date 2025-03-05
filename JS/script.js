a8ae6120aeebc58fafff1658f2a775b98c15b4d7
Claudia Orostica <136994710+Clorostica@users.noreply.github.com>
2025-03-05 14:11:27 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+
+
+        function minimizarVentana() {
+    document.body.style.visibility = 'hidden'; 
+        }
+
+        function cerrarApp() {
+            document.body.style.display = 'none';  
+        }
